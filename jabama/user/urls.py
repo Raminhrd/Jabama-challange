7e722d2eb1c403db74e8b9dfdd5cd0357b8304d6
raminhrd <raminhr.work@gmail.com>
2025-09-10 14:09:35 +0330

    Add_user_reservs_and_paymentJson

--- jabama/user/urls.py
+++ jabama/user/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from user.views import usermanagement
+from user.views import usermanagement , user_reservs , payment
 
 urlpatterns = [
     path('account/' , usermanagement),
+    path('reserve-detail/' , user_reservs),
+    path('payment/' , payment)
 ]
--- jabama/user/views.py
+++ jabama/user/views.py
@@ -1,5 +1,8 @@
 from typing import Union
-from django.http import HttpResponse
+import json
+import os
+from django.conf import settings
+from django.http import HttpResponse , JsonResponse
 
 def usermanagement(request):
     userdetail : Union[str , int]= [
@@ -7,3 +10,19 @@ def usermanagement(request):
     ]
 
     return HttpResponse(userdetail)
+
+def user_reservs(request):
+    reservs_deatil = [
+        {"name" : "Vila kordan" , "date" :'10/6/1404' , "status" : "Reserved"}
+    ]
+
+    return HttpResponse(reservs_deatil)
+
+def payment(request):
+
+    file_path = os.path.join(settings.BASE_DIR, 'user', 'data.json')
+
+    with open(file_path, 'r', encoding='utf-8') as f:
+        data = json.load(f)
+
+    return JsonResponse(data, safe=False)
