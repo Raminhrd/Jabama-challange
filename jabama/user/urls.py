00863dbc38496ee71ec083052e79c3306439b364
raminhrd <raminhr.work@gmail.com>
2025-09-10 16:30:37 +0330

    add_usermanagement_adn_userreserves_json

--- jabama/user/urls.py
+++ jabama/user/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from user.views import usermanagement , user_reservs , payment
+from user.views import usermanagement , usermanagement_json , user_reservs, user_reservs_json , payment
 
 urlpatterns = [
     path('account/' , usermanagement),
+    path('account-json/' , usermanagement_json),
     path('reserve-detail/' , user_reservs),
+    path('reserve-json/' , user_reservs_json),
     path('payment/' , payment)
 ]
--- jabama/user/views.py
+++ jabama/user/views.py
@@ -4,12 +4,23 @@ import os
 from django.conf import settings
 from django.http import HttpResponse , JsonResponse
 
+
 def usermanagement(request):
-    userdetail : Union[str , int]= [
-        {"username" : "srina" , "password" :'srina099489'},
+    userdetail = [
+        {"username": "srina", "password": "srina099489", "telephone": "09338486745"},
     ]
 
-    return HttpResponse(userdetail)
+    result = "<br>".join([f"username: {u['username']} | password: {u['password']} | telephone: {u["telephone"]}" for u in userdetail])
+
+    return HttpResponse(result)
+
+def usermanagement_json(request):
+    userdetail_json = [
+        {"username": "srina", "password": "srina099489", "telephone": "09338486745"},
+    ]
+
+    return JsonResponse(userdetail_json , safe=False)
+
 
 def user_reservs(request):
     reservs_deatil = [
@@ -17,6 +28,13 @@ def user_reservs(request):
     ]
 
     return HttpResponse(reservs_deatil)
+
+def user_reservs_json(request):
+    reservs_deatil_json = [
+        {"name" : "Vila kordan" , "date" :'10/6/1404' , "status" : "Reserved"}
+    ]
+
+    return JsonResponse(reservs_deatil_json , safe=False)
 
 def payment(request):
 
